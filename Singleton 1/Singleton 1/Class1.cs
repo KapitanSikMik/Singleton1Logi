17849d5b00abd777fc67efea06631754f2ef78da
KapitanSikMik <93821633+KapitanSikMik@users.noreply.github.com>
2024-03-10 09:46:11 +0100

    rozwiązanie #1

---
+++ Singleton 1/Singleton 1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_1
+{
+    class Log
+    {
+        private static Log instance = null;
+
+        private Log() { }
+
+
+        public static Log Instance
+        {
+            get
+            {
+                if (instance is null)
+                {
+                    instance = new Log();
+                }
+                return instance;
+            }
+        }
+
+        private List<string> messages = new List<string>();
+
+        public void LogMessage(string message)
+        {
+            messages.Add(message);
+        }
+
+        public List<string> GetLog()
+        {
+            return messages;
+        }
+        
+    }
+}
---
+++ Singleton 1/Singleton 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Instance.LogMessage("rejestr zdarzenie 1");
+            Log.Instance.LogMessage("rejestr zdarzenie 2");
+            Log.Instance.LogMessage("rejestr zdarzenie 3");
+            Log.Instance.LogMessage("rejestr zdarzenie 4");
+
+            List<string> log = Log.Instance.GetLog();
+            
+                Console.WriteLine("Logi:");
+                foreach(string message in log)
+                {
+                    Console.WriteLine(message);
+                }
+            
+            
+        }
+
+        
+        
+    }
+}
